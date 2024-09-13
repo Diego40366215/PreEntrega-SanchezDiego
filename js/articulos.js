b94d31f83bd2a20a1da2b2dec474463e1ddae646
Diego40366215 <diego40366215@outlook.com>
2024-09-13 11:21:35 -0300

    Tercera PreEntrega

---
+++ js/articulos.js
@@ -0,0 +1,48 @@
+
+const articulos = [
+  {
+    id:1,
+    nombre: "ARROZ TRIMACER 500gr.",
+    precio: 1200,
+  },
+  {
+    id:2,
+    nombre: "ÁZUCAR LEDESMA 1k.",
+    precio: 1500,
+  },
+  {
+    id:3,
+    nombre: "CERVEZA HEINEKEN 1l.",
+    precio: 2500,
+  },
+  {
+    id:4,
+    nombre: "CERVEZA QUILMES BAJO CERO 1l.",
+    precio: 2000,
+  },
+  {
+    id:5,
+    nombre: "FIDEOS SPAGUETTIS MATARAZZO 500gr.",
+    precio: 1200,
+  },
+  {
+    id:6,
+    nombre: "FIDEOS TIRABUZÓN MATARAZZO 500gr.",
+    precio: 1200,
+  },
+  {
+    id:7,
+    nombre: "GASEOSA COCA COLA 2.25l.",
+    precio: 3000,
+  },
+  {
+    id:8,
+    nombre: "MAYONESA HELLMANN'S 500gr.",
+    precio: 2300,
+  },
+  {
+    id:9,
+    nombre: "YERBA MATE PLAYADITO 1kg.",
+    precio: 2500,
+  }
+]
